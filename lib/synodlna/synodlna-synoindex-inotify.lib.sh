291613e900f91b48515c546522e0d85063e27696
Didier BONNEFOI <dbonnefoi@gmail.com>
2014-04-15 00:33:18 +0200

    call readlink binary with absolute path to get ipkg version

--- lib/synodlna/synodlna-synoindex-inotify.lib.sh
+++ lib/synodlna/synodlna-synoindex-inotify.lib.sh
@@ -16,6 +16,13 @@ function dlna_inotify_liveupdate
   local sidx_mode=
   local log_message=
 
+  local BIN_READLINK=/opt/bin/readlink
+
+  if [ ! -x "$BIN_READLINK" ]; then
+    echo "$BIN_READLINK not present"
+    return 1
+  fi
+  
   echo "Hum OK, let's catch your activity..."
 
   # separator between field must be a pipe !
@@ -28,23 +35,25 @@ function dlna_inotify_liveupdate
       CLOSE_WRITE,CLOSE|MOVED_TO)    sidx_mode=-a;;
       DELETE|MOVED_FROM)             sidx_mode=-d;;
       DELETE,ISDIR|MOVED_FROM,ISDIR) sidx_mode=-D;;
-      *);; # not catched events
+      *) echo "IGNORING:$Events";; # not catched events
     esac
 
     # break the loop if event not catched
     [ -z "$sidx_mode" ] && continue
 
-    echo "Dir=$Dir | Events=$Events | File=$File"
+    # remove all useless slashes
+    local file_to_index=$($BIN_READLINK -m "${Dir}${File}")
 
-    # remove all useless slashs
-    local file_to_index=$(readlink -m "${Dir}${File}")
     #debug output about synoindex command 
-    echo "$BIN_SYNOINDEX $sidx_mode \"$file_to_index\""
+    if [ "$DEBUG" == "1" ]; then 
+      echo "Dir=$Dir | Events=$Events | File=$File"
+      echo "$BIN_SYNOINDEX $sidx_mode \"$file_to_index\""
+    fi
     # execute the synoindex command
     $BIN_SYNOINDEX $sidx_mode "$file_to_index"
 
     log_message=$(dlna_synoindex_action_detail $sidx_mode)
-    echo "log_message:$log_message"
+    echo "$log_message: $file_to_index"
   done < $IT_FIFO
 
 }
